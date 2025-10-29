8b3654a58c24241a42afc2f58e179697ec2e7dff
majidkambarath <majidmaji3410@gmail.com>
2025-10-29 18:49:10 +0400

    feat: implement Branch management with CRUD operations and routing

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ import inventoryRoutes from "./routes/modules/inventoryRoutes.js";
 import reportsRoutes from "./routes/modules/reportsRoutes.js";
 import userRouter from './routes/user/userRouters.js'
 import classificationRoutes from "./routes/modules/classificationRoutes.js"
+import branchMasterRoutes from "./routes/modules/branchMasterRoutes.js"
 
 
 import { mongodb } from "./config/db.js";
@@ -91,6 +92,8 @@ app.use("/api/v1/fund-transfer", fundTransferRoutes);
 app.use("/api/v1/inventory", inventoryRoutes);
 app.use("/api/v1/reports", reportsRoutes)
 app.use("/api/v1/classification", classificationRoutes)
+app.use("/api/v1/branch", branchMasterRoutes)
+
 
 // Global error handling middleware
 app.use(errorHandler);
