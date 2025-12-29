c6160adb45456f87f6b506c9d2d02fc574eeb57d
Shamil <shamilamiyan@gmail.com>
2025-12-29 09:42:19 +0530

    refactor: remove duplicate metalValue and metalRateValue assignments in AccountFixingService and OpeningFixingService

--- services/modules/OpeningFixingService.js
+++ services/modules/OpeningFixingService.js
@@ -215,7 +215,7 @@ class OpeningFixingService {
                 pureWeight,
                 weightOz,
                 metalRateId,
-                metalValue,
+              
                 bidvalue,
                 metalRateValue,
                 metalValue
